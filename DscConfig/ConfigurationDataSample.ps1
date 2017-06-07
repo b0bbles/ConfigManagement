f00b95393ee984008d3e2ad17a6b2476f9a539c9
b0bbles <mjoverall@gmail.com>
2017-06-07 21:21:24 +0100

    Create ConfigurationDataSample.ps1

---
+++ DscConfig/ConfigurationDataSample.ps1
@@ -0,0 +1,16 @@
+Configuration ConfigurationDataSample
+{
+    Import-DscResource -ModuleName xWebAdministration -Name MSFT_xWebsite
+
+    Write-Verbose $ConfigurationData.NonNodeData.SomeMessage
+
+    Node $AllNodes.Where{$_.Role -eq "WebServer"}.NodeName
+    {
+        xWebsite Site
+        {
+            Name = $Node.SiteName
+            PhysicalPath = $Node.SiteContents
+            Ensure   = "Present"
+        }
+    }
+}
